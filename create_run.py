dcc1ac0f66993436b6413b612148083189c3dac5
VioGarden <111024100+VioGarden@users.noreply.github.com>
2026-01-13 14:10:19 -0500

    set up run

--- create_run.py
+++ create_run.py
@@ -9,14 +9,14 @@ start = time.time()
 output_run_path = os.path.join(RUN_DIR, 'run1')
 
 url_list = []
-url_list.append(("https://files.catbox.moe/cdf1hm.webm", "attack_on_titan"))
-url_list.append(("https://files.catbox.moe/c7wtd4.webm", "spy_x_family"))
-url_list.append(("https://files.catbox.moe/ef2ntg.webm", "demon_slayer"))
-url_list.append(("https://files.catbox.moe/hfybji.webm", "fate_ubw"))
-# url_list.append(("https://files.catbox.moe/p5bjvw.webm", "one_punch_man"))
-# url_list.append(("https://files.catbox.moe/ya38xy.webm", "bungo_stray_dogs"))
+url_list.append(("https://files.catbox.moe/cdf1hm.webm", "attack"))
+url_list.append(("https://files.catbox.moe/c7wtd4.webm", "family"))
+url_list.append(("https://files.catbox.moe/ef2ntg.webm", "ds"))
+url_list.append(("https://files.catbox.moe/hfybji.webm", "work"))
+# url_list.append(("https://files.catbox.moe/p5bjvw.webm", "one"))
+# url_list.append(("https://files.catbox.moe/ya38xy.webm", "dogs"))
 # url_list.append(("https://files.catbox.moe/2f4860.webm", "haikyu"))
-# url_list.append(("https://files.catbox.moe/dcu52b.webm", "re_creators"))
+# url_list.append(("https://files.catbox.moe/dcu52b.webm", "creators"))
 
 for url in url_list:
     response = requests.get(url[0])
